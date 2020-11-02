32d514e39e1c5be28b05b22d32188e32cc6e1a6b
xuae <xuae.git@foxmail.com>
2020-11-02 16:32:38 +0800

    :sparkles: 添加 Button 组件

---
+++ src/utils/Notify.ts
@@ -0,0 +1,3 @@
+export default class Notify {
+  static oneOf() {}
+}
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,12 +2,27 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <x-button color="primary" stylus="text"></x-button>
+  <x-button
+    color="primary"
+    stylus="text"
+    native-type="button"
+    type="fab"
+  ></x-button>
+  <x-button
+    color="primary"
+    stylus="text"
+    native-type="button"
+    type="flat"
+  ></x-button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import XButton from '@/components/Button/XButton.vue';
 
 export default defineComponent({
   name: 'About',
+  components: { XButton },
 });
 </script>
